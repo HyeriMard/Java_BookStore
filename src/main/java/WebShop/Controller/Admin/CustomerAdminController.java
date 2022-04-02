251b3c042f176cc68e55ed4e5ccc91c284def9eb
NTN626 <luarong4444@gmail.com>
2022-04-02 09:30:01 +0700

    upcode

--- src/main/java/WebShop/Controller/Admin/CustomerAdminController.java
+++ src/main/java/WebShop/Controller/Admin/CustomerAdminController.java
@@ -8,41 +8,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import WebShop.Service.User.AccountServiceImpl;
+import WebShop.Service.User.BillDetailServiceImpl;
+import WebShop.Service.User.BillServiceImpl;
 import WebShop.Service.User.CategoryServiceImpl;
 @Controller
 public class CustomerAdminController extends BaseAdminController{
 	@Autowired
 	AccountServiceImpl _accountService;
+	@Autowired
+	BillServiceImpl _BillService;
+	@Autowired
+	BillDetailServiceImpl _BillDetailService;
+	
 	
 	
 	@RequestMapping(value = "/admin/khach-hang")
 	public ModelAndView Index() {
 		
-		
+		if (isLogin()) {
 		mvShare.clear();
 		// lấy danh sách thể loại
 		mvShare.addObject("accounts", _accountService.GetUsersAdmin());
 	
 		mvShare.setViewName("admin/Cumtomer/customer");
-		System.out.print(_accountService.GetUsersAdmin().size());
-		
-	
-		return mvShare;
-	}
-	
-	@RequestMapping(value = "/admin/Cumtomer/{id}", method = RequestMethod.GET)
-	public ModelAndView Detail(@PathVariable long id) {
-		
-	
-		
-		if (isLogin()) {
-			mvShare.clear();
-			mvShare.addObject("bill", _BillService.GetBillByID(id).get(0));
-			mvShare.addObject("detail",_BillDetailService.GetBillDetailByIDBill(id) );
-		
-			mvShare.setViewName("admin/Cumtomer/detailCustomer");
 		}
+		
+	
 		return mvShare;
 	}
+	
+//	@RequestMapping(value = "/admin/Cumtomer/{id}", method = RequestMethod.GET)
+//	public ModelAndView Detail(@PathVariable long id) {
+//		
+//	
+//		if (isLogin()) {
+//			mvShare.clear();
+//			mvShare.addObject("bill", _BillService.GetBillByID(id).get(0));
+//			mvShare.addObject("detail",_BillDetailService.GetBillDetailByIDBill(id) );
+//		
+//			mvShare.setViewName("admin/Cumtomer/detailCustomer");
+//		
+//		return mvShare;
+//	}
 	
 }
